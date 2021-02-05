46769c2eea6e398ca1be3591e776afe7b11f86b6
Greg Schuman <gcschuman@hotmail.com>
2021-02-05 09:53:11 -0800

    Added the script.

---
+++ script.js
@@ -0,0 +1,45 @@
+// Variable declarations
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const replay = document.querySelector('#replay')
+
+// Run animation
+runAnimation()
+
+// Reset DOM function
+function resetDOM () {
+    counter.classList.remove('hide')
+    finalMessage.classList.remove('show')
+
+    nums.forEach((num) => {
+        num.classList.value = ''
+    })
+
+    nums[0].classList.add('in')
+}
+
+// Run animation function
+function runAnimation() {
+    nums.forEach((num, idx) => {
+        const nextToLast = nums.length -1
+
+        num.addEventListener('animationend', (e) => {
+            if (e.animationName === 'goIn' && idx !== nextToLast) {
+                num.classList.remove('in')
+                num.classList.add('out')
+            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+                num.nextElementSibling.classList.add('in')
+            } else{
+                counter.classList.add('hide')
+                finalMessage.classList.add('show')
+            }
+        })
+    })
+}
+
+// Replay event listener
+replay.addEventListener('click', () => {
+    resetDOM()
+    runAnimation()
+})
